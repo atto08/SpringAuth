158e9e5a009b93f9b68c8921da9095e0c0a145d3
atto08 <atto08@naver.com>
2024-07-16 17:31:12 +0900

    learn Spring Security with Jwt

--- src/main/java/com/potado/SpringAuth/service/UserService.java
+++ src/main/java/com/potado/SpringAuth/service/UserService.java
@@ -54,21 +54,4 @@ public class UserService {
         User user = new User(username, password, email, role);
         userRepository.save(user);
     }
-
-    public void login(LoginRequestDto requestDto, HttpServletResponse res) {
-        String username = requestDto.getUsername();
-        String password = requestDto.getPassword();
-
-        // 사용자 확인
-        User user = userRepository.findByUsername(username).orElseThrow(
-                () -> new IllegalArgumentException("등록되지 않은 사용자 입니다.")
-        );
-        // 비밀번호 확인
-        if (!passwordEncoder.matches(password, user.getPassword())) {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-        }
-
-        String token = jwtUtil.createToken(username, user.getRole());
-        jwtUtil.addJwtToCookie(token, res);
-    }
 }
