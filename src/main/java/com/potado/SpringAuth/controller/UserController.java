e40c33c59fa1e12444ecf9e03c38e0f77d0002a0
atto08 <atto08@naver.com>
2024-07-17 17:08:42 +0900

    learn Validation

--- src/main/java/com/potado/SpringAuth/controller/UserController.java
+++ src/main/java/com/potado/SpringAuth/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.potado.SpringAuth.controller;
 
-import com.potado.SpringAuth.dto.LoginRequestDto;
 import com.potado.SpringAuth.dto.SignupRequestDto;
 import com.potado.SpringAuth.service.UserService;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -28,7 +33,16 @@ public class UserController {
     }
 
     @PostMapping("/user/signup")
-    public String signup(SignupRequestDto requestDto){
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";
+        }
+
         userService.signup(requestDto);
 
         return "redirect:/api/user/login-page";
